911567ec85099ab84c11d1653fb525de629dd293
NeZl0R <NeZl0R>
2026-09-20 05:53:32 +0300

    Create application window

--- NADIR/Program.cs
+++ NADIR/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+WindowOptions options = WindowOptions.Default with
+{
+    Size = new Vector2D<int>(1280, 720),
+    Title = "NADIR"
+};
+
+using IWindow window = Window.Create(options);
+
+window.Run();
